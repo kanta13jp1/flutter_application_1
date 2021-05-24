7ce065fcd202f9ace043eb9af6b11fb075877127
kanta13jp1 <kanta13jp@gmail.com>
2021-05-25 08:25:45 +0900

    add _buildSuggestions

--- lib/main.dart
+++ lib/main.dart
@@ -37,4 +37,18 @@ class _RandomWordsState extends State<RandomWords> {
     final wordPair = WordPair.random();
     return Text(wordPair.asPascalCase);
   }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
 }
