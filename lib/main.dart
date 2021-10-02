ac70afbb876d4cce520e5c58bafbce4df413c2df
kanta13jp1 <kanta13jp@gmail.com>
2021-10-02 13:06:40 +0900

    curvedAnimation

--- lib/main.dart
+++ lib/main.dart
@@ -269,10 +269,15 @@ Route _createRoute() {
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       const begin = Offset(0.0, 1.0);
       const end = Offset.zero;
+      const curve = Curves.ease;
+
       final tween = Tween(begin: begin, end: end);
-      final offsetAnimation = animation.drive(tween);
+      final curvedAnimation = CurvedAnimation(
+        parent: animation,
+        curve: curve,
+      );
       return SlideTransition(
-        position: offsetAnimation,
+        position: tween.animate(curvedAnimation),
         child: child,
       );
     },
