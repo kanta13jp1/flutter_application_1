52dfc6f86b08d6ca23ab3f3eec515f7c05a87d4a
kanta13jp1 <kanta13jp@gmail.com>
2021-11-25 01:21:05 +0900

    リファクタリング

---
+++ lib/entity/menstruation.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'menstruation.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Menstruation _$_$_MenstruationFromJson(Map<String, dynamic> json) {
+  return _$_Menstruation(
+    id: json['id'] as String?,
+    beginDate: NonNullTimestampConverter.timestampToDateTime(
+        json['beginDate'] as Timestamp),
+    endDate: NonNullTimestampConverter.timestampToDateTime(
+        json['endDate'] as Timestamp),
+    deletedAt:
+        TimestampConverter.timestampToDateTime(json['deletedAt'] as Timestamp?),
+    createdAt: NonNullTimestampConverter.timestampToDateTime(
+        json['createdAt'] as Timestamp),
+  );
+}
+
+Map<String, dynamic> _$_$_MenstruationToJson(_$_Menstruation instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', toNull(instance.id));
+  val['beginDate'] =
+      NonNullTimestampConverter.dateTimeToTimestamp(instance.beginDate);
+  val['endDate'] =
+      NonNullTimestampConverter.dateTimeToTimestamp(instance.endDate);
+  val['deletedAt'] = TimestampConverter.dateTimeToTimestamp(instance.deletedAt);
+  val['createdAt'] =
+      NonNullTimestampConverter.dateTimeToTimestamp(instance.createdAt);
+  return val;
+}
---
+++ lib/inquiry/inquiry.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_application_1/service/auth.dart';
+import 'package:flutter_application_1/database/database.dart';
+import 'package:flutter_application_1/entity/menstruation.dart';
+import 'package:flutter_application_1/entity/setting.dart';
+import 'package:flutter_application_1/service/menstruation.dart';
+import 'package:flutter_application_1/service/pill_sheet_group.dart';
+import 'package:flutter_application_1/service/setting.dart';
+import 'package:flutter_application_1/util/environment.dart';
+import 'package:package_info/package_info.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+inquiry() {
+  PackageInfo.fromPlatform().then((value) => debugInfo(", ")).then((info) {
+    launch(
+        Uri.encodeFull(
+            "https://docs.google.com/forms/d/e/1FAIpQLSddEpE641jIKEL9cxgiKaRytmBtsP7PXnDdXonEyE-n62JMWQ/viewform?usp=pp_url&entry.2066946565=$info"),
+        forceSafariVC: true);
+  });
+}
+
+Future<String> debugInfo(String separator) async {
+  String userID = (await cacheOrAuth()).uid;
+  DatabaseConnection databaseConnection = DatabaseConnection(userID);
+  final pillSheetGroup =
+      await PillSheetGroupService(databaseConnection).fetchLatest();
+  Setting setting = await SettingService(databaseConnection).fetch();
+  final menstruations =
+      await MenstruationService(databaseConnection).fetchAll();
+  Menstruation? menstruation =
+      menstruations.isNotEmpty ? menstruations.first : null;
+  final package = await PackageInfo.fromPlatform();
+  final appName = package.appName;
+  final buildNumber = package.buildNumber;
+  final packageName = package.packageName;
+  final contents = [
+    "DEBUG INFO",
+    "appName: $appName",
+    "packageName: $packageName",
+    "buildNumber: $buildNumber",
+    "env: ${Environment.isProduction ? "production" : "development"}",
+    "user id: $userID",
+    "latestMenstruation: ${menstruation?.toJson()}",
+    "pillSheetGroupID: ${pillSheetGroup?.id}",
+    "activedPillSheet: ${pillSheetGroup?.activedPillSheet?.toJson()}",
+    "settingState.entity: ${setting.toJson()}",
+  ];
+  return contents.join(separator);
+}
