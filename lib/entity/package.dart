1c4560c8c4a32e63dcc482c59e9df57397abc9a7
kanta13jp1 <kanta13jp@gmail.com>
2021-11-27 13:31:50 +0900

    Package

---
+++ lib/entity/package.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'package.g.dart';
+part 'package.freezed.dart';
+
+abstract class PackageFirestoreKey {
+  static final latestOS = "latestOS";
+}
+
+@freezed
+abstract class Package implements _$Package {
+  factory Package({
+    required String latestOS,
+    required String appName,
+    required String appVersion,
+    required String buildNumber,
+  }) = _Package;
+
+  factory Package.fromJson(Map<String, dynamic> json) =>
+      _$PackageFromJson(json);
+  Map<String, dynamic> toJson() => _$$_PackageToJson(this as _$_Package);
+}
